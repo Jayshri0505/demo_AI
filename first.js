194ed78bb4bed72d777c59a0d8eb6b408c07d782
Jayshri0505 <jayashritechnologia@gmail.com>
2026-02-17 18:19:25 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,20 @@
+console.log("Welcome to javascript");
+console.log("hello");
+fullName="jayashri";
+console.log(fullName);
+//  age=20;
+// age=30;
+// console.log(age); 
+
+let age=20;
+age=30; 
+console.log(age); 
+const PI=3.14;
+console.log(PI);    
+let price=12.34;
+console.log(price);
+yesorno=true;
+
+
+
+
