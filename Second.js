c30f8c033f7eb27dc67c35c4d65ca064d19abf36
Jayshri0505 <jayashritechnologia@gmail.com>
2026-02-18 11:07:27 +0530

    Add files via upload

---
+++ Second.js
@@ -0,0 +1,40 @@
+const student={
+      fullName : "Anjali",
+    age :20,
+    cgpa:8.3 ,
+    isPass: true
+}
+console.log("name of student:-" ,student.fullName);
+console.log(student.cgpa);
+console.log(student.isPass);
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+
+student["fullName"]="Aish";
+console.log(student.fullName);
+ 
+// Arithmetic operator
+let a=5;
+let b=2;
+let c=a+b;
+console.log("a+b=",c);
+// console.log("a-b=",a-b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+// console.log("a++=",a++);
+// console.log("a=",a)
+// console.log("++a=",++a);
+// console.log("a=",a)
+// console.log("--a=",--a);
+// console.log("a--=",a--);
+
+//Assignment Operator
+// let a=5;
+// let b=2;
+// a+=4 //a=a+4
+// console.log("a=",a)
+
+
+
+
